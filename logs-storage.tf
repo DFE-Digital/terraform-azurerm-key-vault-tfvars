f63fa4a03d812e7937daba3a4fc47de9c23dbc64
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-11-23 17:24:01 +0000

    Prevent anonymous blob access

--- logs-storage.tf
+++ logs-storage.tf
@@ -1,14 +1,16 @@
 resource "azurerm_storage_account" "logs" {
   count = local.enable_diagnostic_storage_account ? 1 : 0
 
-  name                      = "${replace(local.resource_prefix, "-", "")}tfvarslogs"
-  resource_group_name       = local.resource_group.name
-  location                  = local.resource_group.location
-  account_tier              = "Standard"
-  account_kind              = "StorageV2"
-  account_replication_type  = "LRS"
-  min_tls_version           = "TLS1_2"
-  enable_https_traffic_only = true
+  name                            = "${replace(local.resource_prefix, "-", "")}tfvarslogs"
+  resource_group_name             = local.resource_group.name
+  location                        = local.resource_group.location
+  account_tier                    = "Standard"
+  account_kind                    = "StorageV2"
+  account_replication_type        = "LRS"
+  min_tls_version                 = "TLS1_2"
+  enable_https_traffic_only       = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
 
   tags = local.tags
 }
